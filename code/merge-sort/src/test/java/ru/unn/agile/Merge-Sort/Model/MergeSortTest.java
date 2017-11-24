b6028832a57dc2c165b3fd96ca058120ecb2d334
KseniaBastrakova <bastrakova.kseinia@gmail.com>
2017-11-24 23:26:22 +0300

    Added hashcode method to Car class, fixed equals method in Car class

--- code/merge-sort/src/test/java/ru/unn/agile/Merge-Sort/Model/MergeSortTest.java
+++ code/merge-sort/src/test/java/ru/unn/agile/Merge-Sort/Model/MergeSortTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 import java.util.Arrays;
+import java.lang.Object;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,7 +47,7 @@ public class MergeSortTest {
 
     @Test
     public void multipleValuesTwoDegreeSizeIntAscending() {
-        Integer[] values = {1 , 11, 56, 999, -2, 18, -1999, 3, 4, 15, 24, 87, 13, 44, 77, 100};
+        Integer[] values = {1, 11, 56, 999, -2, 18, -1999, 3, 4, 15, 24, 87, 13, 44, 77, 100};
         ArrayList<Integer> input = new ArrayList<Integer>(Arrays.asList(values));
         Integer[] expectedValues = {-1999, -2, 1, 3, 4, 11,
                 13, 15, 18, 24, 44, 56, 77, 87,  100, 999};
@@ -59,7 +60,7 @@ public class MergeSortTest {
     public void multipleValuesDoubleAscending() {
         Double[] values = {1., 10., 4.5, 54.01, 9., 0., 2., 3.9, 4.8};
         ArrayList<Double> input = new ArrayList<Double>(Arrays.asList(values));
-        Double[] expectedValues = {0., 1., 2., 3.9, 4.5, 4.8, 9., 10., 54.01,};
+        Double[] expectedValues = {0., 1., 2., 3.9, 4.5, 4.8, 9., 10., 54.01};
         ArrayList<Double> expected = new ArrayList<Double>(Arrays.asList(expectedValues));
         ArrayList<Double> actual = MergeSort.ascendingSort(input);
         assertEquals(expected, actual);
@@ -78,10 +79,10 @@ public class MergeSortTest {
 
     class Car implements Comparable<Car> {
         private int maxSpeed;
-        public Car( int theMaxSpeed ){
+        Car( int theMaxSpeed ) {
             maxSpeed = theMaxSpeed;
         }
-        public int compareTo(Car first) {
+        public int compareTo(final Car first) {
             if (first.maxSpeed == maxSpeed) {
                 return 0;
             }
@@ -92,19 +93,26 @@ public class MergeSortTest {
             }
         }
         @Override
-        public boolean equals(Object obj) {
-            if (obj == this) return true;
-            if (obj == null || obj.getClass() != this.getClass())
+            public int hashCode() {
+                return maxSpeed;
+            }
+        @Override
+        public boolean equals(final Object obj) {
+            if (obj == this) {
+                return true;
+            }
+            if (obj == null || obj.getClass() != this.getClass()) {
                 return false;
+            }
             return (this.maxSpeed == ((Car) obj).maxSpeed);
         }
     }
 
     @Test
     public void customTypeTwoValuesComparatorAscending() {
-        Car[] values = new Car[]{ new Car(400), new Car(30)};
+        Car[] values = new Car[]{ new Car(400), new Car(30) };
         ArrayList<Car> input = new ArrayList<Car>(Arrays.asList(values));
-        Car[] valuesExpected = new Car[]{ new Car(30),new Car(400) };
+        Car[] valuesExpected = new Car[]{ new Car(30), new Car(400) };
         ArrayList<Car> expected = new ArrayList<Car>(Arrays.asList(valuesExpected));
         ArrayList<Car> sortValues = MergeSort.ascendingSort(input);
         assertEquals(expected,sortValues);
@@ -112,7 +120,7 @@ public class MergeSortTest {
 
     @Test
     public void customTypeMultipleValuesAscending() {
-        Car[] values = new Car[]{ new Car(30), new Car(400),
+        Car[] values = new Car[] { new Car(30), new Car(400),
                        new Car(50), new Car(1), new Car(13),
                        new Car(777) };
         ArrayList<Car> input = new ArrayList<Car>(Arrays.asList(values));
@@ -122,7 +130,7 @@ public class MergeSortTest {
                                new Car(777) };
         ArrayList<Car> expected = new ArrayList<Car>(Arrays.asList(valuesExpected));
         ArrayList<Car> sortValues = MergeSort.ascendingSort(input);
-        assertEquals(expected,sortValues);
+        assertEquals(expected, sortValues);
     }
 
     @Test
@@ -154,7 +162,7 @@ public class MergeSortTest {
 
     @Test
     public void multipleValuesIntDescending() {
-        Integer[] values = {1 , 11, 6, -7, -2, 18, 1999, 3, 4};
+        Integer[] values = {1, 11, 6, -7, -2, 18, 1999, 3, 4};
         ArrayList<Integer> input = new ArrayList<Integer>(Arrays.asList(values));
         Integer[] expectedValues = {1999, 18, 11, 6, 4, 3, 1, -2, -7};
         ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(expectedValues));
@@ -195,7 +203,7 @@ public class MergeSortTest {
 
     @Test
     public void customTypeTwoValuesDecending() {
-        Car[] values = new Car[]{ new Car(404), new Car(56)};
+        Car[] values = new Car[]{ new Car(404), new Car(56) };
         ArrayList<Car> input = new ArrayList<Car>(Arrays.asList(values));
         Car[] valuesExpected = new Car[]{new Car(404), new Car(56)};
         ArrayList<Car> expected = new ArrayList<Car>(Arrays.asList(valuesExpected));
@@ -216,6 +224,6 @@ public class MergeSortTest {
         ArrayList<Car> expected = new ArrayList<Car>(Arrays.asList(valuesExpected));
 
         ArrayList<Car> sortValues = MergeSort.descendingSort(input);
-        assertEquals(expected,sortValues);
+        assertEquals(expected, sortValues);
     }
 }
