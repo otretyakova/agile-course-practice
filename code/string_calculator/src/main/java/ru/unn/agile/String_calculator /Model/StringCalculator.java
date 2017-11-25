f100733d1f4058bb6ffa1789ea6f666681ab7e09
viktor roy <viktor.roy94@mail.ru>
2017-11-25 17:48:34 +0300

    added lab1

---
+++ code/string_calculator/src/main/java/ru/unn/agile/String_calculator /Model/StringCalculator.java
@@ -0,0 +1,57 @@
+package ru.unn.agile.string_calculator.model;
+
+public class StringCalculator {
+    public int add(final String input) {
+        if ("".equals(input)) {
+            return 0;
+        }
+        String str = input;
+        String delimiter = ",\n";
+        String firstSymbol = input.substring(0, 1);
+        if (firstSymbol.matches("[,.:;]")) {
+            delimiter += firstSymbol;
+            str = input.substring(2);
+        }
+
+        String[] numbers = str.split("[" + delimiter + "]");
+
+        checkIncorrectData(numbers);
+        checkNegatives(numbers);
+
+        int sum = 0;
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+        return sum;
+    }
+
+    private void checkNegatives(final String[] numbers) {
+        String error = "Negatives ";
+        for (String number: numbers) {
+            if (Integer.parseInt(number) < 0) {
+                error += number + " ";
+            }
+        }
+        if (!"Negatives ".equals(error)) {
+            error += "not allowed";
+            throw new RuntimeException(error);
+        }
+    }
+
+    private void checkIncorrectData(final String[] numbers) {
+        for (String number: numbers) {
+            if ("".equals(number) || !isNumeric(number)) {
+                throw new RuntimeException("Incorrect data");
+            }
+        }
+    }
+
+    private boolean isNumeric(final String str) {
+        try {
+            Integer.parseInt(str);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ code/string_calculator/src/test/java/ru/unn/agile/String_calculator/Model/StringCalculatorTest.java
@@ -0,0 +1,113 @@
+package ru.unn.agile.string_calculator.model;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+public class StringCalculatorTest {
+    private StringCalculator calc;
+
+    @Before
+    public void setUp() {
+        calc = new StringCalculator();
+    }
+
+    @Test
+    public void stringWithoutNumbers() {
+        assertAddReturns(0, "");
+    }
+
+    @Test
+    public void stringWithOneNumber() {
+        assertAddReturns(1, "1");
+    }
+
+    @Test
+    public void stringWithTwoNumbers() {
+        assertAddReturns(3, "1,2");
+    }
+
+    @Test
+    public void stringWithThreeNumbers() {
+        assertAddReturns(6, "1,2,3");
+    }
+
+    @Test
+    public void stringWithFourNumbers() {
+        assertAddReturns(10, "1,2,3,4");
+    }
+
+    @Test
+    public void stringWithALotOfNumbers() {
+        assertAddReturns(210, "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20");
+    }
+
+    @Test
+    public void stringWithNewLineSymbol() {
+        assertAddReturns(6, "1\n2,3");
+    }
+
+    @Test
+    public void stringWithNewDelimiter() {
+        assertAddReturns(6, ";\n1;2;3");
+    }
+
+    @Test
+    public void stringWithDotDelimiter() {
+        assertAddReturns(6, ".\n1.2.3");
+    }
+
+    @Test
+    public void stringWithDotDelimiterAndNewLineSymbol() {
+        assertAddReturns(6, ".\n1\n2.3");
+    }
+
+    @Test
+    public void stringWithNegativeNumbers() {
+        try {
+            assertAddReturns(6, "-1,2,3");
+        } catch (RuntimeException e) {
+            assertEquals("Negatives -1 not allowed", e.getMessage());
+        }
+    }
+
+    @Test
+    public void stringWithTwoNegativeNumbers() {
+        try {
+            assertAddReturns(6, "-1,2,-3");
+        } catch (RuntimeException e) {
+            assertEquals("Negatives -1 -3 not allowed", e.getMessage());
+        }
+    }
+
+    @Test
+    public void stringWithAllNegativeNumbers() {
+        try {
+            assertAddReturns(6, "-1,-2,-3");
+        } catch (RuntimeException e) {
+            assertEquals("Negatives -1 -2 -3 not allowed", e.getMessage());
+        }
+    }
+
+    @Test
+    public void stringWithIncorrectData() {
+        try {
+            assertAddReturns(6, "sadfasdf");
+        } catch (RuntimeException e) {
+            assertEquals("Incorrect data", e.getMessage());
+        }
+    }
+
+    @Test
+    public void stringWithMistake() {
+        try {
+            assertAddReturns(6, "1,2,3s");
+        } catch (RuntimeException e) {
+            assertEquals("Incorrect data", e.getMessage());
+        }
+    }
+
+    private void assertAddReturns(final int expected, final String input) {
+        assertEquals(expected, calc.add(input));
+    }
+}
