4acc0c6aec2424438eb6e45b5e64627ee04d6983
Anatoliy Rozanov <anatoliy.rozanov@intel.com>
2017-11-25 23:43:13 +0300

    Fixing comments by Victor Roy

--- code/numbers-in-words/src/main/java/ru/unn/agile/NumbersInWords/Model/NumbersInWordsConverter.java
+++ code/numbers-in-words/src/main/java/ru/unn/agile/NumbersInWords/Model/NumbersInWordsConverter.java
@@ -6,45 +6,45 @@ import java.util.Map;
 
 public class NumbersInWordsConverter {
 
-    private Map<Character, String> digits;
-    private Map<Character, String> twoDigitNumbersBeforeTwenty;
-    private Map<Character, String> twoDigitNumbersAfterTwenty;
+    private Map<String, String> digits;
+    private Map<String, String> twoDigitNumbersBeforeTwenty;
+    private Map<String, String> twoDigitNumbersAfterTwenty;
     private Map<Integer, String> digitPosition;
 
     public NumbersInWordsConverter() {
-        digits = new HashMap<Character, String>();
-        digits.put('0', "zero");
-        digits.put('1', "one");
-        digits.put('2', "two");
-        digits.put('3', "three");
-        digits.put('4', "four");
-        digits.put('5', "five");
-        digits.put('6', "six");
-        digits.put('7', "seven");
-        digits.put('8', "eight");
-        digits.put('9', "nine");
+        digits = new HashMap<String, String>();
+        digits.put("0", "zero");
+        digits.put("1", "one");
+        digits.put("2", "two");
+        digits.put("3", "three");
+        digits.put("4", "four");
+        digits.put("5", "five");
+        digits.put("6", "six");
+        digits.put("7", "seven");
+        digits.put("8", "eight");
+        digits.put("9", "nine");
 
-        twoDigitNumbersBeforeTwenty = new HashMap<Character, String>();
-        twoDigitNumbersBeforeTwenty.put('0', "ten");
-        twoDigitNumbersBeforeTwenty.put('1', "eleven");
-        twoDigitNumbersBeforeTwenty.put('2', "twelve");
-        twoDigitNumbersBeforeTwenty.put('3', "thirteen");
-        twoDigitNumbersBeforeTwenty.put('4', "fourteen");
-        twoDigitNumbersBeforeTwenty.put('5', "fifteen");
-        twoDigitNumbersBeforeTwenty.put('6', "sixteen");
-        twoDigitNumbersBeforeTwenty.put('7', "seventeen");
-        twoDigitNumbersBeforeTwenty.put('8', "eighteen");
-        twoDigitNumbersBeforeTwenty.put('9', "nineteen");
+        twoDigitNumbersBeforeTwenty = new HashMap<String, String>();
+        twoDigitNumbersBeforeTwenty.put("0", "ten");
+        twoDigitNumbersBeforeTwenty.put("1", "eleven");
+        twoDigitNumbersBeforeTwenty.put("2", "twelve");
+        twoDigitNumbersBeforeTwenty.put("3", "thirteen");
+        twoDigitNumbersBeforeTwenty.put("4", "fourteen");
+        twoDigitNumbersBeforeTwenty.put("5", "fifteen");
+        twoDigitNumbersBeforeTwenty.put("6", "sixteen");
+        twoDigitNumbersBeforeTwenty.put("7", "seventeen");
+        twoDigitNumbersBeforeTwenty.put("8", "eighteen");
+        twoDigitNumbersBeforeTwenty.put("9", "nineteen");
 
-        twoDigitNumbersAfterTwenty = new HashMap<Character, String>();
-        twoDigitNumbersAfterTwenty.put('2', "twenty");
-        twoDigitNumbersAfterTwenty.put('3', "thirty");
-        twoDigitNumbersAfterTwenty.put('4', "forty");
-        twoDigitNumbersAfterTwenty.put('5', "fifty");
-        twoDigitNumbersAfterTwenty.put('6', "sixty");
-        twoDigitNumbersAfterTwenty.put('7', "seventy");
-        twoDigitNumbersAfterTwenty.put('8', "eighty");
-        twoDigitNumbersAfterTwenty.put('9', "ninety");
+        twoDigitNumbersAfterTwenty = new HashMap<String, String>();
+        twoDigitNumbersAfterTwenty.put("2", "twenty");
+        twoDigitNumbersAfterTwenty.put("3", "thirty");
+        twoDigitNumbersAfterTwenty.put("4", "forty");
+        twoDigitNumbersAfterTwenty.put("5", "fifty");
+        twoDigitNumbersAfterTwenty.put("6", "sixty");
+        twoDigitNumbersAfterTwenty.put("7", "seventy");
+        twoDigitNumbersAfterTwenty.put("8", "eighty");
+        twoDigitNumbersAfterTwenty.put("9", "ninety");
 
         final int milliardPosition = 3;
         final int millionPosition = 2;
@@ -100,8 +100,8 @@ public class NumbersInWordsConverter {
         int i = processedNumber.length() - subNumberLength;
         String subNumber = processedNumber.substring(i, i + subNumberLength);
         convertedNumber = convertThreeDigitNumber(subNumber);
-        boolean isOneOrTwoDigitSubNumber = convertedNumber != null
-                    && !convertedNumber.equals("") && (subNumber.charAt(0) == '0');
+        boolean isOneOrTwoDigitSubNumber = !isStringsEqual(convertedNumber, "")
+                && (subNumber.charAt(0) == '0');
         if (isOneOrTwoDigitSubNumber) {
             convertedNumber = "and " + convertedNumber;
         }
@@ -110,8 +110,8 @@ public class NumbersInWordsConverter {
         for (i = processedNumber.length() - 2 * subNumberLength; i >= 0; i -= subNumberLength) {
             subNumber = processedNumber.substring(i, i + subNumberLength);
             String convertedThreeDigitNumber = convertThreeDigitNumber(subNumber);
-            if (convertedThreeDigitNumber != null && !convertedThreeDigitNumber.equals("")) {
-                if (convertedNumber != null && !convertedNumber.equals("")) {
+            if (!isStringsEqual(convertedThreeDigitNumber, "")) {
+                if (!isStringsEqual(convertedNumber, "")) {
                     convertedNumber = convertedThreeDigitNumber + " "
                             + convertDigitPosition(position) + " " + convertedNumber;
                 } else {
@@ -127,7 +127,7 @@ public class NumbersInWordsConverter {
     private String convertDecimalPart(final String number) throws IllegalArgumentException {
         String convertedNumber = " point";
         for (int i = 0; i < number.length(); i++) {
-            convertedNumber += " " + convertOneDigitNumber(number.charAt(i));
+            convertedNumber += " " + convertOneDigitNumber(number.substring(i, i + 1));
         }
         return convertedNumber;
     }
@@ -143,7 +143,7 @@ public class NumbersInWordsConverter {
         return newString;
     }
 
-    private String convertOneDigitNumber(final char digit) {
+    private String convertOneDigitNumber(final String digit) {
         String convertedDigit = digits.get(digit);
         if (convertedDigit == null) {
             throw new IllegalArgumentException("Input number contains incorrect symbols");
@@ -151,36 +151,27 @@ public class NumbersInWordsConverter {
         return convertedDigit;
     }
 
-    private String convertOneDigitNumber(final String number) throws IllegalArgumentException {
-        char firstDigit = number.charAt(0);
-        return convertOneDigitNumber(firstDigit);
-    }
-
-    private String convertTwoDigitNumber(final char firstDigit,
-                                         final char secondDigit) throws IllegalArgumentException {
+    private String convertTwoDigitNumber(final String number) throws IllegalArgumentException {
         String convertedNumber = "";
-        if (firstDigit == '1') {
+        String firstDigit = number.substring(0, 1);
+        String secondDigit = number.substring(1, 2);
+
+        if (isStringsEqual(firstDigit, "1")) {
             convertedNumber = convertTwoDigitNumberBeforeTwenty(secondDigit);
-        } else if (firstDigit != '0') {
+        } else if (!isStringsEqual(firstDigit, "0")) {
             String twoDigitNumber = twoDigitNumbersAfterTwenty.get(firstDigit);
             if (twoDigitNumber == null) {
                 throw new IllegalArgumentException("Input number contains incorrect symbols");
             }
             convertedNumber = twoDigitNumber + " " + convertOneDigitNumber(secondDigit);
-        } else if (firstDigit == '0' && secondDigit != '0') {
+        } else if (isStringsEqual(firstDigit, "0") && !isStringsEqual(secondDigit, "0")) {
             convertedNumber = convertOneDigitNumber(secondDigit);
         }
 
         return convertedNumber;
     }
 
-    private String convertTwoDigitNumber(final String number) throws IllegalArgumentException {
-        char firstDigit = number.charAt(0);
-        char secondDigit = number.charAt(1);
-        return convertTwoDigitNumber(firstDigit, secondDigit);
-    }
-
-    private String convertTwoDigitNumberBeforeTwenty(final char secondDigit)
+    private String convertTwoDigitNumberBeforeTwenty(final String secondDigit)
             throws IllegalArgumentException {
         String convertedNumber = twoDigitNumbersBeforeTwenty.get(secondDigit);
         if (convertedNumber == null) {
@@ -189,12 +180,22 @@ public class NumbersInWordsConverter {
         return convertedNumber;
     }
 
-    private String convertThreeDigitNumber(final char firstDigit, final char secondDigit,
-                                           final char thirdDigit) throws IllegalArgumentException {
+    private String convertThreeDigitNumber(final String number) throws IllegalArgumentException {
         String convertedNumber = "";
-        String convertedTwoDigitNumber = convertTwoDigitNumber(secondDigit, thirdDigit);
-        if (firstDigit != '0') {
-            if (convertedTwoDigitNumber != null && convertedTwoDigitNumber.equals("")) {
+
+        final int startOfFirstDigit = 0;
+        final int startOfSecondDigit = 1;
+        final int startOfThirdDigit = 2;
+        final int endOfFirstDigit = 1;
+        final int endOfSecondDigit = 2;
+        final int endOfThirdDigit = 3;
+        String firstDigit = number.substring(startOfFirstDigit, endOfFirstDigit);
+        String secondDigit = number.substring(startOfSecondDigit, endOfSecondDigit);
+        String thirdDigit = number.substring(startOfThirdDigit, endOfThirdDigit);
+
+        String convertedTwoDigitNumber = convertTwoDigitNumber(secondDigit + thirdDigit);
+        if (!isStringsEqual(firstDigit, "0")) {
+            if (isStringsEqual(convertedTwoDigitNumber, "")) {
                 convertedNumber = convertOneDigitNumber(firstDigit) + " hundred";
             } else {
                 convertedNumber = convertOneDigitNumber(firstDigit)
@@ -206,19 +207,16 @@ public class NumbersInWordsConverter {
         return convertedNumber;
     }
 
-    private String convertThreeDigitNumber(final String number) throws IllegalArgumentException {
-        char firstDigit = number.charAt(0);
-        char secondDigit = number.charAt(1);
-        char thirdDigit = number.charAt(2);
-        return convertThreeDigitNumber(firstDigit, secondDigit, thirdDigit);
-    }
-
     private String convertDigitPosition(final int position) throws IllegalArgumentException {
         String convertedPosition = digitPosition.get(position);
         if (convertedPosition == null) {
             throw new IllegalArgumentException("Input number is too big or too small");
         }
         return convertedPosition;
+    }
+
+    boolean isStringsEqual(final String str, final String expected) {
+        return str != null && str.equals(expected);
     }
 
 }
