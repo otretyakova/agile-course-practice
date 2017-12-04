4538d095659280bbcb4af66799366458e75d8ed7
dsadova <daria_sadova@mail.ru>
2017-12-04 16:04:40 +0300

    Fixed Markdown syntax

--- code/sadova-daria/src/main/java/ru/unn/agile/Vector3d/Model/Vector3d.java
+++ code/sadova-daria/src/main/java/ru/unn/agile/Vector3d/Model/Vector3d.java
@@ -1,9 +1,9 @@
 package ru.unn.agile.vectors.model;
 
 public class Vector3d {
-private double x;
-private double y;
-private double z;
+    private double x;
+    private double y;
+    private double z;
 
     public double getX() {
         return this.x;
@@ -17,30 +17,33 @@ private double z;
         return this.z;
     }
 
-public Vector3d(final double x, final double y, final double z) {
-   this.x = x;
-   this.y = y;
-   this.z = z;
+    public Vector3d(final double x, final double y, final double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
-public double vectorLength() {
+    public double vectorLength() {
         return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2) + Math.pow(this.z, 2));
     }
 
-public Vector3d normalize() {
-    double len = vectorLength();
-    return new Vector3d(x / len, y / len, z / len);
+    public Vector3d normalize() {
+        if (isVectorValid()) {
+            double len = vectorLength();
+            return new Vector3d(x / len, y / len, z / len);
+        }
+        return new Vector3d(0, 0, 0);
     }
 
-public double dotProduct(final Vector3d first) {
+    public double dotProduct(final Vector3d first) {
         return (x * first.x + y * first.y + z * first.z);
     }
 
-public Vector3d crossProduct(final Vector3d first) {
-    double newX = y * first.z - z * first.y;
-    double newY = -(x * first.z - z * first.x);
-    double newZ = x * first.y - y * first.x;
-    return new Vector3d(newX, newY, newZ);
+    public Vector3d crossProduct(final Vector3d first) {
+        double newX = y * first.z - z * first.y;
+        double newY = -(x * first.z - z * first.x);
+        double newZ = x * first.y - y * first.x;
+        return new Vector3d(newX, newY, newZ);
     }
 
     private boolean isVectorValid() {
@@ -50,6 +53,7 @@ public Vector3d crossProduct(final Vector3d first) {
     public boolean equalComplitely(final Vector3d first) {
         return (getX() == first.getX()) && (getY() == first.getY()) && (getZ() == first.getZ());
     }
+
     public boolean equalNormalized(final Vector3d first) {
         return normalize().equalComplitely(first.normalize());
     }
--- code/sadova-daria/src/test/java/ru/unn/agile/Vectors3d/Model/Vector3dTest.java
+++ code/sadova-daria/src/test/java/ru/unn/agile/Vectors3d/Model/Vector3dTest.java
@@ -6,33 +6,41 @@ import static org.junit.Assert.*;
 public class Vector3dTest {
     private final double delta = 1e-6;
 
-@Test
-public void canCreateVector3d() {
-    Vector3d vector = new Vector3d(1, 1, 1);
-    assertNotNull(vector);
-}
+    @Test
+    public void canCreateVector3d() {
+        Vector3d vector = new Vector3d(1, 1, 1);
+        assertNotNull(vector);
+    }
+
     @Test
     public void canCreateNegativeVector3d() {
         Vector3d vector = new Vector3d(-1, 2, -1);
         assertNotNull(vector);
     }
-@Test
-public void canCreateVector3dWithZeroValues() {
-    Vector3d vector = new Vector3d(0, 0, 0);
-    assertNotNull(vector);
-}
+    @Test
+    public void canCreateVector3dWithZeroValues() {
+        Vector3d vector = new Vector3d(0, 0, 0);
+        assertNotNull(vector);
+    }
 
-@Test
-public void canLengthBeFromZeroVector() {
-    Vector3d vector = new Vector3d(0, 0, 0);
-    double len = vector.vectorLength();
-    assertEquals(0, len, delta);
-}
+    @Test
+    public void canLengthBeFromZeroVector() {
+        Vector3d vector = new Vector3d(0, 0, 0);
+        double len = vector.vectorLength();
+        assertEquals(0, len, delta);
+    }
     @Test
     public void canFindLength() {
         Vector3d vector = new Vector3d(0, 3, 4);
         double len = vector.vectorLength();
         assertEquals(5, len, delta);
+    }
+
+    @Test
+    public void canNormalizeZeroVector() {
+        Vector3d vector = new Vector3d(0, 0, 0);
+        Vector3d normVector = vector.normalize();
+        assertTrue(normVector.equalComplitely(vector));
     }
 
     @Test
@@ -53,6 +61,14 @@ public void canLengthBeFromZeroVector() {
     @Test
     public void canFindDotProductWith90DegreesBetweenThem() {
         Vector3d first = new Vector3d(1, 2, 3);
+        Vector3d second = new Vector3d(1, 4, -3);
+        double dotProd = first.dotProduct(second);
+        assertTrue(0 == dotProd);
+    }
+
+    @Test
+    public void canFindDotProductWithMultiplingByZero() {
+        Vector3d first = new Vector3d(0, 0, 0);
         Vector3d second = new Vector3d(1, 4, -3);
         double dotProd = first.dotProduct(second);
         assertTrue(0 == dotProd);
@@ -83,6 +99,15 @@ public void canLengthBeFromZeroVector() {
         Vector3d result = new Vector3d(3, 6, -3);
         Vector3d crossProd = first.crossProduct(second);
         assertFalse(result.equalComplitely(crossProd));
+    }
+
+    @Test
+    public void canFindCrossProductWithMultiplingByZero() {
+        Vector3d first = new Vector3d(0, 0, 0);
+        Vector3d second = new Vector3d(1, 2, 3);
+        Vector3d result = new Vector3d(0, 0, 0);
+        Vector3d crossProd = first.crossProduct(second);
+        assertTrue(result.equalComplitely(crossProd));
     }
 }
 
