7392ffe8a608d3887bd08b3b2cad00718d993918
Gorbunova Natalya <natashenka351@gmail.com>
2017-12-14 02:06:26 +0300

    fixed comments

--- code/mortgage-calculator/src/main/java/ru/unn/agile/MortgageCalculator/Model/MortgageCalculator.java
+++ code/mortgage-calculator/src/main/java/ru/unn/agile/MortgageCalculator/Model/MortgageCalculator.java
@@ -5,36 +5,38 @@ import java.util.List;
 
 import static java.lang.Math.pow;
 import static java.lang.Math.round;
+import static ru.unn.agile.MortgageCalculator.Model.MortgageCalculator.periodType.month;
+import static ru.unn.agile.MortgageCalculator.Model.MortgageCalculator.periodType.year;
 
 public class MortgageCalculator {
     public static final int MONTH_IN_YEAR = 12;
+
+    public enum periodType {month, year}
+
     private float amount;
     private int period;
-    private String periodType;
     private float rate;
     private List<Integer> payments;
     private int payment;
     private String error;
 
-    public MortgageCalculator(final float amount, final int period, final String periodType,
-                              final float rate) {
+    public MortgageCalculator(final float amount, final int period, final float rate) {
         this.amount = amount;
         this.period = period;
-        this.periodType = periodType;
         this.rate = rate;
         payments = new ArrayList<Integer>();
     }
 
-    public List<Integer> differentiatedPayment() {
+    public List<Integer> differentiatedPayment(periodType t) {
         int n = 0;
         float amountT = amount;
         if (period != 0) {
             if (rate != 0) {
-                switch (periodType) {
-                    case "month":
+                switch (t) {
+                    case month:
                         n = period;
                         break;
-                    case "year":
+                    case year:
                         n = period * MONTH_IN_YEAR;
                         break;
                     default:
@@ -56,15 +58,15 @@ public class MortgageCalculator {
         }
     }
 
-    public int annuityPayment() {
+    public int annuityPayment(periodType t) {
         int n = period;
         if (period != 0) {
             if (rate != 0) {
-                if (periodType != null && !periodType.equals("month")
-                        && !periodType.equals("year")) {
+                if (t != null && !t.equals(month)
+                        && !t.equals(year)) {
                     error = "Type of period is undefined";
                     return 0;
-                } else if (periodType != null && periodType.equals("year")) {
+                } else if (t != null && t.equals(year)) {
                     n *= MONTH_IN_YEAR;
                 }
                 payment = (int) (amount * (((rate / MONTH_IN_YEAR) * pow(1 + rate / MONTH_IN_YEAR,
@@ -80,22 +82,16 @@ public class MortgageCalculator {
         }
     }
 
-    public int printPayments(final String paymentType, final int periodNumber) {
-        int pay = 0;
+    public int getPayments(final String paymentType, final int periodNumber, periodType t) {
         switch (paymentType) {
             case "differentiated":
-                payments = differentiatedPayment();
-                pay = payments.get(periodNumber);
-                break;
+                return differentiatedPayment(t).get(periodNumber);
             case "annuity":
-                payment = annuityPayment();
-                pay = payment;
-                break;
+                return annuityPayment(t);
             default:
                 error = "Can't print payment of undefined type of payment";
                 return 0;
         }
-        return pay;
     }
 
     public String getError() {
--- code/mortgage-calculator/src/test/java/ru/unn/agile/MortgageCalculator/Model/MortgageCalculatorTest.java
+++ code/mortgage-calculator/src/test/java/ru/unn/agile/MortgageCalculator/Model/MortgageCalculatorTest.java
@@ -4,76 +4,78 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static ru.unn.agile.MortgageCalculator.Model.MortgageCalculator.periodType.month;
+import static ru.unn.agile.MortgageCalculator.Model.MortgageCalculator.periodType.year;
 
 public class MortgageCalculatorTest {
     @Test
     public void canCalculateDifferentiatedMortgage() {
-        MortgageCalculator calc = new MortgageCalculator(1000, 3, "month", (float) 0.05);
-        assertEquals(Arrays.asList(338, 336, 335), calc.differentiatedPayment());
+        MortgageCalculator calc = new MortgageCalculator(1000, 3, (float) 0.05);
+        assertEquals(Arrays.asList(338, 336, 335), calc.differentiatedPayment(month));
     }
 
     @Test
     public void canCalculateAnnuityMortgage() {
-        MortgageCalculator calc = new MortgageCalculator(1000, 3, "month", (float) 0.05);
-        assertEquals(336, calc.annuityPayment());
+        MortgageCalculator calc = new MortgageCalculator(1000, 3, (float) 0.05);
+        assertEquals(336, calc.annuityPayment(month));
     }
 
     @Test
     public void canCalculateDifferentiatedMortgageWithYearPeriodType() {
-        MortgageCalculator calc = new MortgageCalculator(100000, 1, "year", (float) 0.05);
+        MortgageCalculator calc = new MortgageCalculator(100000, 1, (float) 0.05);
         assertEquals(Arrays.asList(8750, 8715, 8681, 8646, 8611, 8576, 8542, 8507,
-                8472, 8437, 8403, 8368), calc.differentiatedPayment());
+                8472, 8437, 8403, 8368), calc.differentiatedPayment(year));
     }
 
     @Test
     public void canCalculateAnnuityMortgageWithYearPeriodType() {
-        MortgageCalculator calc = new MortgageCalculator(100000, 1, "year", (float) 0.05);
-        assertEquals(8560, calc.annuityPayment());
+        MortgageCalculator calc = new MortgageCalculator(100000, 1, (float) 0.05);
+        assertEquals(8560, calc.annuityPayment(year));
     }
 
     @Test
-    public void canPrintPaymentForDifferentiatedMortgage() {
-        MortgageCalculator calc = new MortgageCalculator(1000, 3, "month", (float) 0.05);
-        assertEquals(336, calc.printPayments("differentiated", 1));
+    public void canGetPaymentForDifferentiatedMortgage() {
+        MortgageCalculator calc = new MortgageCalculator(1000, 3, (float) 0.05);
+        assertEquals(336, calc.getPayments("differentiated", 1, month));
     }
 
     @Test
     public void canReturnZeroWhenCalculateDifferentiatedMortgageWithZeroAmount() {
-        MortgageCalculator calc = new MortgageCalculator(0, 3, "month", (float) 0.05);
-        assertEquals(Arrays.asList(0, 0, 0), calc.differentiatedPayment());
+        MortgageCalculator calc = new MortgageCalculator(0, 3, (float) 0.05);
+        assertEquals(Arrays.asList(0, 0, 0), calc.differentiatedPayment(month));
     }
 
     @Test
     public void canReturnZeroWhenCalculateAnnuityMortgageWithZeroAmount() {
-        MortgageCalculator calc = new MortgageCalculator(0, 3, "month", (float) 0.05);
-        assertEquals(0, calc.annuityPayment());
+        MortgageCalculator calc = new MortgageCalculator(0, 3, (float) 0.05);
+        assertEquals(0, calc.annuityPayment(month));
     }
 
     @Test
-    public void canPrintPaymentForDifferentiatedMortgageWithZeroAmount() {
-        MortgageCalculator calc = new MortgageCalculator(0, 3, "month", (float) 0.05);
-        assertEquals(0, calc.printPayments("differentiated", 1));
+    public void canGetPaymentForDifferentiatedMortgageWithZeroAmount() {
+        MortgageCalculator calc = new MortgageCalculator(0, 3, (float) 0.05);
+        assertEquals(0, calc.getPayments("differentiated", 1, month));
     }
 
     @Test
     public void canCatchErrorWhenCalculateDifferentiatedMortgageWithZeroPeriod() {
-        MortgageCalculator calc = new MortgageCalculator(1000, 0, "month", (float) 0.05);
-        calc.differentiatedPayment();
+        MortgageCalculator calc = new MortgageCalculator(1000, 0, (float) 0.05);
+        calc.differentiatedPayment(month);
         assertEquals("Period is zero", calc.getError());
     }
 
     @Test
     public void canCatchErrorWhenCalculateAnnuityMortgageWithZeroPeriod() {
-        MortgageCalculator calc = new MortgageCalculator(1000, 0, "month", (float) 0.05);
-        calc.annuityPayment();
+        MortgageCalculator calc = new MortgageCalculator(1000, 0, (float) 0.05);
+        calc.annuityPayment(month);
         assertEquals("Period is zero", calc.getError());
     }
 
-    @Test
+    /*@Test
     public void canCatchErrorWhenCalculateDifferentiatedMortgageWithUndefinedPeriodType() {
         MortgageCalculator calc = new MortgageCalculator(1000, 12, "abcd", (float) 0.05);
-        calc.differentiatedPayment();
+        calc.differentiatedPayment(abcd);
         assertEquals("Type of period is undefined", calc.getError());
     }
 
@@ -82,26 +84,26 @@ public class MortgageCalculatorTest {
         MortgageCalculator calc = new MortgageCalculator(1000, 12, "abcd", (float) 0.05);
         calc.annuityPayment();
         assertEquals("Type of period is undefined", calc.getError());
-    }
+    }*/
 
     @Test
     public void canCatchErrorWhenCalculateDifferentiatedMortgageWithZeroRate() {
-        MortgageCalculator calc = new MortgageCalculator(1000, 12, "month", 0);
-        calc.differentiatedPayment();
+        MortgageCalculator calc = new MortgageCalculator(1000, 12, 0);
+        calc.differentiatedPayment(month);
         assertEquals("Rate is zero", calc.getError());
     }
 
     @Test
     public void canCatchErrorWhenCalculateAnnuityMortgageWithZeroRate() {
-        MortgageCalculator calc = new MortgageCalculator(1000, 12, "month", 0);
-        calc.annuityPayment();
+        MortgageCalculator calc = new MortgageCalculator(1000, 12, 0);
+        calc.annuityPayment(month);
         assertEquals("Rate is zero", calc.getError());
     }
 
     @Test
-    public void canPrintPaymentForDifferentiatedMortgageWithUndefinedTypeOfPayment() {
-        MortgageCalculator calc = new MortgageCalculator(1000, 3, "month", (float) 0.05);
-        calc.printPayments("abcde", 1);
+    public void canGetPaymentForDifferentiatedMortgageWithUndefinedTypeOfPayment() {
+        MortgageCalculator calc = new MortgageCalculator(1000, 3, (float) 0.05);
+        calc.getPayments("abcde", 1, month);
         assertEquals("Can't print payment of undefined type of payment", calc.getError());
     }
 }
