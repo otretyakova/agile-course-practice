f2428a41fff1fe030b84c33c397ad951669ef125
Vershinina Olga <olya.olya-ver@yandex.ru>
2018-01-06 14:50:57 +0300

    Added binding logic logging to View

--- code/string-calculator/View/src/main/java/ru/unn/agile/StringCalculator/view/Calculator.java
+++ code/string-calculator/View/src/main/java/ru/unn/agile/StringCalculator/view/Calculator.java
@@ -1,14 +1,29 @@
 package ru.unn.agile.StringCalculator.view;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import ru.unn.agile.StringCalculator.viewmodel.ViewModel;
+import ru.unn.agile.StringCalculator.infrastructure.TxtLogger;
 
 public class Calculator {
     @FXML
     void initialize() {
+        viewModel.setLogger(new TxtLogger("./TxtLogger.log"));
+
+        final ChangeListener<Boolean> focusChangeListener = new ChangeListener<Boolean>() {
+            @Override
+            public void changed(final ObservableValue<? extends Boolean> observable,
+                                final Boolean oldValue, final Boolean newValue) {
+                viewModel.onFocusChanged(oldValue, newValue);
+            }
+        };
+
         txtInputString.textProperty().bindBidirectional(viewModel.inputStringProperty());
+        txtInputString.focusedProperty().addListener(focusChangeListener);
+
         btnCalc.setOnAction(event -> viewModel.calculate());
     }
 
