de98cffdd65b9299dc662f0d0d07173b5a39bb29
Krivonosov Mikhail <mike_live@mail.ru>
2018-01-09 20:09:31 +0300

    Added empty Infrastructure.

---
+++ code/left_sided_heap/Infrastructure/src/test/java/ru/unn/agile/LeftSidedHeap/infrastructure/ViewModelWithTxtLoggerTests.java
@@ -0,0 +1,14 @@
+package ru.unn.agile.LeftSidedHeap.infrastructure;
+/*
+import ru.unn.agile.ComplexNumber.viewmodel.ViewModel;
+import ru.unn.agile.ComplexNumber.viewmodel.ViewModelTests;
+
+public class ViewModelWithTxtLoggerTests extends ViewModelTests {
+    @Override
+    public void setUp() {
+        TxtLogger realLogger =
+            new TxtLogger("./ViewModel_with_TxtLogger_Tests-lab3.log");
+        super.setExternalViewModel(new ViewModel(realLogger));
+    }
+}
+*/
