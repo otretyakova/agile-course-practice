c6ed99be03a97edcf198920bd8e778ca7735e139
vihuhol <semichev9@gmail.com>
2018-01-21 19:57:59 +0300

    Demetra law in ViewModelTests fixed and some tests added

--- code/merge-sort/ViewModel/src/test/java/ru/unn/agile/mergesort/viewmodel/ViewModelTests.java
+++ code/merge-sort/ViewModel/src/test/java/ru/unn/agile/mergesort/viewmodel/ViewModelTests.java
@@ -1,6 +1,5 @@
 package ru.unn.agile.mergesort.viewmodel;
 
-import javafx.beans.property.StringProperty;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,8 +12,6 @@ public class ViewModelTests {
     @Before
     public void setUp() {
         viewModel = new ViewModel();
-        input = viewModel.inputProperty();
-        status = viewModel.statusProperty();
     }
 
     @After
@@ -24,106 +21,148 @@ public class ViewModelTests {
 
     @Test
     public void canSetDefaultValues() {
-        assertEquals("", input.get());
-        assertEquals("", viewModel.outputProperty().get());
-        assertEquals(Status.WAITING.toString(), status.get());
+        assertEquals("", getInput());
+        assertEquals("", getOutput());
+        assertEquals(Status.WAITING.toString(), getStatus());
     }
 
     @Test
     public void statusIsWaitingWhenCalculateWithEmptyInput() {
         viewModel.calculate();
 
-        assertEquals(Status.WAITING.toString(), status.get());
+        assertEquals(Status.WAITING.toString(), getStatus());
     }
 
     @Test
     public void statusIsWaitingWithInputWithSpacesOnly() {
-        input.set("   ");
+        setInput("   ");
 
-        assertEquals(Status.WAITING.toString(), status.get());
+        assertEquals(Status.WAITING.toString(), getStatus());
     }
 
     @Test
     public void statusIsReadyWhenInputIsFilledByIntegers() {
-        input.set("15 4 42 8 16 23");
+        setInput("15 4 42 8 16 23");
 
-        assertEquals(Status.READY.toString(), status.get());
+        assertEquals(Status.READY.toString(), getStatus());
     }
 
     @Test
     public void statusIsWaitingAfterInputErase() {
-        input.set("15 4 42 8 16 23");
-        input.set("");
+        setInput("15 4 42 8 16 23");
+        setInput("");
 
-        assertEquals(Status.WAITING.toString(), status.get());
+        assertEquals(Status.WAITING.toString(), getStatus());
     }
 
     @Test
     public void statusIsBadFormatWhenInputHasWrongFormat() {
-        input.set("How this program works???");
+        setInput("How this program works???");
 
-        assertEquals(Status.BAD_FORMAT.toString(), status.get());
+        assertEquals(Status.BAD_FORMAT.toString(), getStatus());
     }
 
     @Test
     public void statusIsBadFormatWhenInputHasNotANumberWord() {
-        input.set("15 4 42 Oops 8 16 23");
+        setInput("15 4 42 Oops 8 16 23");
 
-        assertEquals(Status.BAD_FORMAT.toString(), status.get());
+        assertEquals(Status.BAD_FORMAT.toString(), getStatus());
+    }
+
+    @Test
+    public void statusIsBadFormatWhenInputHasTooBigNumbers() {
+        setInput("15 4 42 853418926487349812743 8 16 23");
+
+        assertEquals(Status.BAD_FORMAT.toString(), getStatus());
     }
 
     @Test
     public void statusIsReadyAfterBadFormatInputIsFixed() {
-        input.set("How this program works???");
-        input.set("15 4 42 8 16 23");
+        setInput("How this program works???");
+        setInput("15 4 42 8 16 23");
 
-        assertEquals(Status.READY.toString(), status.get());
+        assertEquals(Status.READY.toString(), getStatus());
     }
 
     @Test
     public void statusIsSuccessAfterCalculation() {
-        input.set("15 4 42 8 16 23");
+        setInput("15 4 42 8 16 23");
         viewModel.calculate();
 
-        assertEquals(Status.SUCCESS.toString(), status.get());
+        assertEquals(Status.SUCCESS.toString(), getStatus());
     }
 
     @Test
     public void sortButtonIsDisabledInitially() {
-        assertTrue(viewModel.sortDisabledProperty().get());
+        assertTrue(sortIsDisable());
     }
 
     @Test
     public void sortButtonIsDisabledWhenFormatIsBad() {
-        input.set("15 4 42 Oops 8 16 23");
+        setInput("15 4 42 Oops 8 16 23");
 
-        assertTrue(viewModel.sortDisabledProperty().get());
+        assertTrue(sortIsDisable());
     }
 
     @Test
     public void sortButtonIsEnabledWithCorrectInput() {
-        input.set("15 4 42 8 16 23");
+        setInput("15 4 42 8 16 23");
 
-        assertFalse(viewModel.sortDisabledProperty().get());
+        assertFalse(sortIsDisable());
     }
 
     @Test
     public void outputIsCorrectAfterSortCorrectInput() {
-        input.set("15 4 42 8 16 23");
+        setInput("15 4 42 8 16 23");
         viewModel.calculate();
 
-        assertEquals("4 8 15 16 23 42", viewModel.outputProperty().get());
+        assertEquals("4 8 15 16 23 42", getOutput());
     }
 
     @Test
-    public void outputIsCorrectWithAbnormalSpacePlacing() {
-        input.set("  15 4    42  8 16 23  ");
+    public void outputIsCorrectWithAbnormalSpacePlacingInInput() {
+        setInput("  15 4    42  8 16 23  ");
         viewModel.calculate();
 
-        assertEquals("4 8 15 16 23 42", viewModel.outputProperty().get());
+        assertEquals("4 8 15 16 23 42", getOutput());
+    }
+
+    @Test
+    public void outputIsCorrectWithNegativeIntegersInInput() {
+        setInput("1 2 -3 4 -5");
+        viewModel.calculate();
+
+        assertEquals("-5 -3 1 2 4", getOutput());
+    }
+
+    @Test
+    public void outputIsCorrectWithIntegersWithPlusInInput() {
+        setInput("15 +4 42 8 +16 23");
+        viewModel.calculate();
+
+        assertEquals("4 8 15 16 23 42", getOutput());
+    }
+
+
+    private void setInput(final String inputStr) {
+        viewModel.inputProperty().set(inputStr);
+    }
+
+    private String getInput() {
+        return viewModel.inputProperty().get();
+    }
+
+    private String getStatus() {
+        return viewModel.statusProperty().get();
+    }
+
+    private String getOutput() {
+        return viewModel.outputProperty().get();
+    }
+
+    private boolean sortIsDisable() {
+        return viewModel.sortDisabledProperty().get();
     }
 
     private ViewModel viewModel;
-    private StringProperty input;
-    private StringProperty status;
 }
