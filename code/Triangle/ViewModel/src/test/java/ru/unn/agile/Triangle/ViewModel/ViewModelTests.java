9666804afe91f2ebd574f0a0bd5f11682df5e872
dsadova <daria_sadova@mail.ru>
2018-01-23 18:03:56 +0300

    figured out with travis

--- code/Triangle/ViewModel/src/test/java/ru/unn/agile/Triangle/ViewModel/ViewModelTests.java
+++ code/Triangle/ViewModel/src/test/java/ru/unn/agile/Triangle/ViewModel/ViewModelTests.java
@@ -10,7 +10,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
 
 public class ViewModelTests {
     private ViewModel viewModel;
